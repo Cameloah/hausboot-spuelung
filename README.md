e1f57054497d487d836eba88d032640dca6fc08a
Cameloah <koor.jones@gmail.com>
2024-10-29 19:22:20 +0100

    version tagged v0.1.0

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # hausboot-spuelung
-Managing the relays timings for a pump and a water solenoid valve for a maritime toilet
+Managing the relays timings for a pump and a water solenoid valve for a marine toilet
--- include/version.h
+++ include/version.h
@@ -1,11 +1,11 @@
 // define your current FW version here
 #define FW_VERSION_MAJOR                    0
-#define FW_VERSION_MINOR                    0
-#define FW_VERSION_PATCH                    2
+#define FW_VERSION_MINOR                    1
+#define FW_VERSION_PATCH                    0
 
 // set the github user and source repo here
-#define GITHUB_REPO     "your-user/your-repository"
+#define GITHUB_REPO     "Cameloah/hausboot-spuelung"
 // enter the name of the firmware bin file here. A <version> string will be replaced by the FW version
-#define GITHUB_FW_BIN   "project_<version>.bin"
+#define GITHUB_FW_BIN   "spuelung_<version>.bin"
 // enter the name of the compiled filesystem binary here
 #define GITHUB_FS_BIN   "spiffs.bin"
